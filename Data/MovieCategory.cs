a025af829ec0ec457a2c95b5acce12ef49720488
Jongbo Lee <ganett33@gmail.com>
2022-12-15 12:16:44 +1300

    Add Models(Actor, Cinema, Movie, Producer) and Data (MovieCategory)

---
+++ Data/MovieCategory.cs
@@ -0,0 +1,21 @@
+﻿namespace eTickets.Data
+{
+    public enum MovieCategory
+    {
+        Action,
+        Adventure,
+        Animated,
+        Comedy,
+        Drama,
+        Fantasy,
+        Historical,
+        Horror,
+        Musical,
+        Mystery,
+        Romance,
+        ScienceFiction,
+        Thriller,
+        Western
+
+    }
+}
