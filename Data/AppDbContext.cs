22de8556f1cc1ed6ffd26c0e6a5ace9da30c769b
Jongbo Lee <ganett33@gmail.com>
2022-12-15 13:41:12 +1300

    Add dbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContext<AppDbContext> options) : base(options) 
+        {
+        }
+    }
+}
